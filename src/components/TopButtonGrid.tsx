f2d5a26cb3d5f3ee9cbbab92b55185be2725ea2b
yuumi7372 <morita.yumi707@mail.kyutech.jp>
2024-12-06 12:47:52 +0900

    トップページに大幅な変更を加えました

--- src/components/TopButtonGrid.tsx
+++ src/components/TopButtonGrid.tsx
@@ -1,13 +1,62 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import Link from 'next/link';
 
 const buttonLabels = [
-  { label: '集卵 count', path: '/keisha', illustrationSrc: '/images/count-egg.jpg' },
-  { label: 'サイズ size', path: '/size', illustrationSrc: '/images/size-egg.jpg' },
-  { label: '鶏 chicken', path: '/chicken', illustrationSrc: '/images/chicken.jpg' }
+  { 
+    label: '集卵 count', 
+    path: '/keisha', 
+    illustrationSrc1: '/images/count-egg.jpg',//通常時の画像
+    illustrationSrc2: '/images/hakari.png',//ホバー時の画像
+  },
+  { 
+    label: 'サイズ size',
+    path: '/size', 
+    illustrationSrc1: '/images/size-egg.jpg',//通常時の画像
+    illustrationSrc2: '/images/hakari.png',//ホバー時の画像
+  },
+  { 
+    label: '鶏 chicken', 
+    path: '/chicken', 
+    illustrationSrc1: '/images/chicken.jpg',
+    illustrationSrc2: '/images/hakari.png',
+  },
 ];
 
 const TopButtonGrid: React.FC = () => {
+  //状態管理のフック
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [imageSrc, setImageSrc] = useState<string>('');
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    //マウスホバー中の処理
+    if(hoveredIndex !== null){
+      setImageSrc(buttonLabels[hoveredIndex].illustrationSrc1);//初期画面
+
+      //画像を交互に切り替え
+      interval = setInterval(() => {
+        setImageSrc((prev) => {
+          return prev === buttonLabels[hoveredIndex].illustrationSrc1
+            ? buttonLabels[hoveredIndex].illustrationSrc2
+            : buttonLabels[hoveredIndex].illustrationSrc1
+        });
+      }, 500); //0.5秒ごとに切り替え
+    } else {
+      // ホバーしていないときは、最初の画像を表示
+      setImageSrc('');
+      if(interval){
+        clearInterval(interval);
+      }
+    }
+
+    return () => {
+      if(interval){
+        clearInterval(interval);
+      }
+    };
+  }, [hoveredIndex]);
+
   return (
     <div
       style={{
@@ -24,11 +73,13 @@ const TopButtonGrid: React.FC = () => {
       {buttonLabels.map((label, index) => (
         <div
           key={index}
+          onMouseEnter={() => setHoveredIndex(index)} //ホバー開始時
+          onMouseLeave={() => setHoveredIndex(null)} //ホバー終了時
         >
           {/* 画像をクリック可能にする */}
           <Link href={label.path} passHref>
             <img
-              src={label.illustrationSrc}
+              src={hoveredIndex === index ? imageSrc : label.illustrationSrc1}
               alt={label.label}
               style={{
                 width: '140px', // アイコンの幅を大きく
@@ -36,6 +87,7 @@ const TopButtonGrid: React.FC = () => {
                 borderRadius: '10px', // アイコンに角をつける
                 marginBottom: '15px', // アイコンとボタン間に隙間を追加
                 cursor: 'pointer', // 画像をクリック可能に
+                transition: '0.3s ease',
               }}
             />
           </Link>
