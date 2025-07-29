1e879bb6f0e9d710e23a5de5bfddb95f51c36b70
mii <korondog777@gmail.com>
2025-07-29 21:50:43 +0900

    単語の追加

--- kokkofactory/backend/app/db/supabase.py
+++ kokkofactory/backend/app/db/supabase.py
@@ -1,9 +1,9 @@
 #supabase clientの初期化
 import os
-from supabase import create_Client,Client
+from supabase import create_client,Client
 from dotenv import load_dotenv
 
-load.dotenv()
+load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
