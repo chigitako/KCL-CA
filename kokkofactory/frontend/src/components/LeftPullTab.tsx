bb3b9fcd90517b6ce7eb3c6957ea33b72a40427e
mii <korondog777@gmail.com>
2025-12-13 06:29:33 +0900

    産卵予測

--- kokkofactory/frontend/src/components/LeftPullTab.tsx
+++ kokkofactory/frontend/src/components/LeftPullTab.tsx
@@ -48,6 +48,7 @@ const LeftPullTab = ({ children }: { children: React.ReactNode }) => {
         <NavButton href="/web/shipment">出荷履歴</NavButton>
         <NavButton href="/web/customers">取引先名簿</NavButton>
         <NavButton href="/web/stock">在庫</NavButton>
+        <NavButton href="/web/prediction">予測</NavButton>
       </div>
 
       {/* ページ本体 */}
