a7f1c0dacbb6e04422d34a52c60d21c9baf5c29c
yuumi7372 <morita.yumi707@mail.kyutech.jp>
2026-01-28 21:20:30 +0900

    プルタブ「マーケティング」→「経営サポート」

--- kokkofactory/frontend/src/components/LeftPullTab.tsx
+++ kokkofactory/frontend/src/components/LeftPullTab.tsx
@@ -89,7 +89,7 @@ const LeftPullTab = ({ children }: { children: React.ReactNode }) => {
         <NavButton href="/web/customers">取引先名簿</NavButton>
         <NavButton href="/web/stock">在庫</NavButton>
         <NavButton href="/web/prediction">予測</NavButton>
-        <NavButton href="/web/marketing">マーケティング</NavButton>
+        <NavButton href="/web/marketing">経営サポート</NavButton>
         <LogoutButton className={styles.buttonStyle} />
       </div>
 
