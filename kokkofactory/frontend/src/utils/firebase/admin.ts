bba33784222072d6162e1b964d917fd88e5e4ab8
mii <korondog777@gmail.com>
2026-01-30 14:26:52 +0900

    admin.tsの修正

--- kokkofactory/frontend/src/utils/firebase/admin.ts
+++ kokkofactory/frontend/src/utils/firebase/admin.ts
@@ -5,7 +5,6 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      // 改行コードを正しく処理するために .replace してね！
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
